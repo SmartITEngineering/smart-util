4cea95bb7482daf41b4ba6211f73eb0ce922f560
Imran M Yousuf <imyousuf@smartitengineering.com>
2009-01-19 13:51:42 +0600

    Add spring util module structure
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ smart-util/smart-spring-util/src/main/java/com/smartitengineering/util/spring/App.java
@@ -0,0 +1,13 @@
+package com.smartitengineering.util.spring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
