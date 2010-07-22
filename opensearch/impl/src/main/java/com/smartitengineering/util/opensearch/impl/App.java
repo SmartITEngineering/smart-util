140f2048f31306263dfe2615772ac879f398101e
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-07-22 13:24:16 +0600

    Add modules for OpenSearch Description Document
    The module will be responsible for producing and parsing OpenSearch
    Description documents. Primarily it will produce the document. Parsing will
    be added later.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ opensearch/impl/src/main/java/com/smartitengineering/util/opensearch/impl/App.java
@@ -0,0 +1,13 @@
+package com.smartitengineering.util.opensearch.impl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
