7748cd6d2a9915d3eee931f172bd8ab2f0dc33ce
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-08-01 20:56:15 +0600

    Add Image implementation and builder
    Implement Image as per the specification. URL is must and the rest are
    optional.
    
    The builder is mainly there to abstract the implementation and allow users
    to use chaining to create an image.
    
    Removed the dummy App.java as it was no good!
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ opensearch/impl/src/main/java/com/smartitengineering/util/opensearch/impl/ImageBuilder.java
@@ -0,0 +1,71 @@
+/*
+ * This is a utility project for wide range of applications
+ *
+ * Copyright (C) 2010  Imran M Yousuf (imyousuf@smartitengineering.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  10-1  USA
+ */
+package com.smartitengineering.util.opensearch.impl;
+
+import com.smartitengineering.util.opensearch.api.Image;
+import java.net.URI;
+
+/**
+ * A builder that can be used to build an image
+ * @author imyousuf
+ */
+public class ImageBuilder {
+
+  private URI imageUri;
+  private int height, width;
+  private String mimeType;
+
+  public static ImageBuilder getImageBuilder(URI imageUri) {
+    return new ImageBuilder(imageUri);
+  }
+
+  protected ImageBuilder(URI imageUri) {
+    imageUri(imageUri);
+  }
+
+  public Image build() {
+    ImageImpl image = new ImageImpl(imageUri);
+    image.setHeight(height);
+    image.setWidth(width);
+    image.setMimeType(mimeType);
+    return image;
+  }
+
+  public ImageBuilder height(int height) {
+    this.height = height;
+    return this;
+  }
+
+  public ImageBuilder width(int width) {
+    this.width = width;
+    return this;
+  }
+
+  public ImageBuilder mimeType(String mimeType) {
+    this.mimeType = mimeType;
+    return this;
+  }
+
+  public ImageBuilder imageUri(URI imageUri) {
+    if (imageUri == null) {
+      throw new IllegalArgumentException("Image URI can not be null!");
+    }
+    this.imageUri = imageUri;
+    return this;
+  }
+}
