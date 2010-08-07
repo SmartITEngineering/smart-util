3e1aa65f5d862a757ef93b09354b8e4f841313ef
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-08-07 09:34:36 +0600

    Add server side util modules structure
    One module is for generic server side utilities and the other for Atom
    feed specific server side utilities.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ rest/server/src/main/java/com/smartitengineering/smartutil/rest/server/App.java
@@ -0,0 +1,13 @@
+package com.smartitengineering.smartutil.rest.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
