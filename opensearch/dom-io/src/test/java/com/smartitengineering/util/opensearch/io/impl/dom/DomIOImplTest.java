95fcec6cc09f9e4f0dfd3f167178118dee615623
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-08-11 13:53:40 +0600

    Make the descriptors constant so that they are reachable from other modules
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- opensearch/dom-io/src/test/java/com/smartitengineering/util/opensearch/io/impl/dom/DomIOImplTest.java
+++ opensearch/dom-io/src/test/java/com/smartitengineering/util/opensearch/io/impl/dom/DomIOImplTest.java
@@ -32,11 +32,11 @@ import junit.framework.TestCase;
  */
 public class DomIOImplTest extends TestCase {
 
-  static final String MIN = "<?xml version=\"1.0\"?>\n<OpenSearchDescription xmlns=\"http://a9.com/-/spec/opensearch/" +
+  public static final String MIN = "<?xml version=\"1.0\"?>\n<OpenSearchDescription xmlns=\"http://a9.com/-/spec/opensearch/" +
       "1.1/\"><ShortName>testDesc</ShortName><Description>Some test description</Description><Url template=" +
       "\"http://google.com/\" type=\"text/html\" rel=\"results\" indexOffset=\"1\" pageOffset=\"1\" /><Syndication" +
       "Right>open</SyndicationRight><AdultContent>false</AdultContent><Language>en</Language></OpenSearchDescription>\n";
-  static final String MAX = "<?xml version=\"1.0\"?>\n<OpenSearchDescription xmlns=\"http://a9.com/-/spec/opensearch/" +
+  public static final String MAX = "<?xml version=\"1.0\"?>\n<OpenSearchDescription xmlns=\"http://a9.com/-/spec/opensearch/" +
       "1.1/\"><ShortName>testDesc</ShortName><Description>Some test description</Description><Tags>tag1 tag2</Tags>" +
       "<Contact>contact</Contact><Url template=\"http://google.com/\" type=\"text/html\" rel=\"results\" indexOffset=" +
       "\"1\" pageOffset=\"1\" /><Url template=\"http://yahoo.com/\" type=\"text/html\" rel=\"self collection\" " +
@@ -48,13 +48,27 @@ public class DomIOImplTest extends TestCase {
       "Engineering Ltd. - Beta Team</Developer><Attribution>Some attribution</Attribution><SyndicationRight>open" +
       "</SyndicationRight><AdultContent>false</AdultContent><Language>en-us</Language><OutputEncoding>ISO-8859-1" +
       "</OutputEncoding><InputEncoding>UTF-8</InputEncoding></OpenSearchDescription>\n";
+  public static OpenSearchDescriptor MIN_DESC = OpenSearchAPIBuilders.getOpenSearchDescriptorBuilder().shortName(
+      "testDesc").description("Some test description").urls(OpenSearchAPIBuilders.getUrlBuilder().template(
+      "http://google.com/").type("text/html").build()).language(Locale.ENGLISH).build();
+  public static OpenSearchDescriptor MAX_DESC = OpenSearchAPIBuilders.getOpenSearchDescriptorBuilder().shortName(
+      "testDesc").description("Some test description").urls(OpenSearchAPIBuilders.getUrlBuilder().template(
+      "http://google.com/").type("text/html").build(), OpenSearchAPIBuilders.getUrlBuilder().template(
+      "http://yahoo.com/").type("text/html").rel(RelEnum.SELF).rel(RelEnum.COLLECTION).build()).tags("tag1", "tag2").
+      contact("contact").developer("Smart IT Engineering Ltd. - Beta Team").longName("All combo test").attribution(
+      "Some attribution").language(Locale.US).inputEncoding("UTF-8").outputEncoding("ISO-8859-1").images(OpenSearchAPIBuilders.
+      getImageBuilder().imageUri("http://google.com/logo.gif").build(), OpenSearchAPIBuilders.getImageBuilder().imageUri(
+      "http://yahoo.com/logo.gif").height(100).width(200).mimeType("image/jpg").build()).queries(OpenSearchAPIBuilders.
+      getQueryBuilder().searchTerms("some terms").totalResults(-2).role(OpenSearchAPIBuilders.getRoleBuilder().localRole(
+      LocalOpenSearchRoles.SUPERSET).build()).build(), OpenSearchAPIBuilders.getQueryBuilder().searchTerms("any term").
+      count(3).startIndex(0).startPage(3).totalResults(2).customAttribute("xmlns:cns", "http://google.com/1.1").
+      customAttribute("cns:add", "val").role(OpenSearchAPIBuilders.getRoleBuilder().extendedRole("cns:arole").build()).
+      inputEncodings("ASCII").outputEncodings("UTF-16").language(Locale.GERMAN.getLanguage()).title(
+      "comprehensive query").build()).build();
 
   public void testMinimalWrite() {
-    OpenSearchDescriptor descriptor = OpenSearchAPIBuilders.getOpenSearchDescriptorBuilder().shortName("testDesc").
-        description("Some test description").urls(OpenSearchAPIBuilders.getUrlBuilder().template("http://google.com/").
-        type("text/html").build()).language(Locale.ENGLISH).build();
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    OpenSearchDescriptorWriter writer = new OpenSearchDescriptorWriter(outputStream, descriptor, true);
+    OpenSearchDescriptorWriter writer = new OpenSearchDescriptorWriter(outputStream, MIN_DESC, true);
     try {
       final long start = System.currentTimeMillis();
       writer.write();
@@ -71,23 +85,8 @@ public class DomIOImplTest extends TestCase {
   }
 
   public void testWrite() {
-    OpenSearchDescriptor descriptor = OpenSearchAPIBuilders.getOpenSearchDescriptorBuilder().shortName("testDesc").
-        description("Some test description").urls(OpenSearchAPIBuilders.getUrlBuilder().template("http://google.com/").
-        type("text/html").build(), OpenSearchAPIBuilders.getUrlBuilder().template("http://yahoo.com/").type("text/html").
-        rel(RelEnum.SELF).rel(RelEnum.COLLECTION).build()).tags("tag1", "tag2").contact("contact").developer(
-        "Smart IT Engineering Ltd. - Beta Team").longName("All combo test").attribution("Some attribution").language(
-        Locale.US).inputEncoding("UTF-8").outputEncoding("ISO-8859-1").images(OpenSearchAPIBuilders.getImageBuilder().
-        imageUri("http://google.com/logo.gif").build(), OpenSearchAPIBuilders.getImageBuilder().imageUri(
-        "http://yahoo.com/logo.gif").height(100).width(200).mimeType("image/jpg").build()).queries(OpenSearchAPIBuilders.
-        getQueryBuilder().searchTerms("some terms").totalResults(-2).role(OpenSearchAPIBuilders.getRoleBuilder().
-        localRole(LocalOpenSearchRoles.SUPERSET).build()).build(), OpenSearchAPIBuilders.getQueryBuilder().searchTerms(
-        "any term").count(3).startIndex(0).startPage(3).totalResults(2).customAttribute("xmlns:cns",
-                                                                                        "http://google.com/1.1").
-        customAttribute("cns:add", "val").role(OpenSearchAPIBuilders.getRoleBuilder().extendedRole("cns:arole").build()).
-        inputEncodings("ASCII").outputEncodings("UTF-16").language(Locale.GERMAN.getLanguage()).title(
-        "comprehensive query").build()).build();
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    OpenSearchDescriptorWriter writer = new OpenSearchDescriptorWriter(outputStream, descriptor, true);
+    OpenSearchDescriptorWriter writer = new OpenSearchDescriptorWriter(outputStream, MAX_DESC, true);
     try {
       final long start = System.currentTimeMillis();
       writer.write();
