4d92c9b8de7ad87c5e94ab232de129bfc3df7933
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-09-03 23:09:32 +0600

    Use HTTPCache4J to make GET requests and jersey apache client handler for rest
    This change should work fine in terms of caching but need to test how the
    missing codes for GET from Jersey Apache Client Handler will effect the
    proceedings
    File to check for important missings.
    https://jersey.dev.java.net/source/browse/jersey/trunk/jersey/contribs/jersey-apache-client/src/main/java/com/sun/jersey/client/apache/ApacheHttpClientHandler.java?rev=4088&view=markup
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- rest/jersey-cache-client/src/main/java/com/smartitengineering/util/rest/client/jersey/cache/CacheableClientHandler.java
+++ rest/jersey-cache-client/src/main/java/com/smartitengineering/util/rest/client/jersey/cache/CacheableClientHandler.java
@@ -22,7 +22,23 @@ import com.sun.jersey.api.client.ClientRequest;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.TerminatingClientHandler;
 import com.sun.jersey.client.apache.ApacheHttpClientHandler;
+import com.sun.jersey.core.header.InBoundHeaders;
+import com.sun.jersey.core.util.ReaderWriter;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import org.apache.commons.httpclient.HttpClient;
+import org.codehaus.httpcache4j.HTTPRequest;
+import org.codehaus.httpcache4j.HTTPResponse;
+import org.codehaus.httpcache4j.Header;
+import org.codehaus.httpcache4j.Headers;
+import org.codehaus.httpcache4j.cache.CacheStorage;
+import org.codehaus.httpcache4j.cache.HTTPCache;
+import org.codehaus.httpcache4j.cache.MemoryCacheStorage;
+import org.codehaus.httpcache4j.client.HTTPClientResponseResolver;
 
 /**
  *
@@ -32,18 +48,82 @@ public class CacheableClientHandler
     extends TerminatingClientHandler {
 
   private ApacheHttpClientHandler apacheHttpClientHandler;
+  private HTTPCache cache;
 
   public CacheableClientHandler(HttpClient httpClient) {
+    this(httpClient, new MemoryCacheStorage());
+  }
+
+  public CacheableClientHandler(HttpClient httpClient,
+                                CacheStorage storage) {
     apacheHttpClientHandler = new ApacheHttpClientHandler(httpClient);
+    cache = new HTTPCache(storage, new HTTPClientResponseResolver(httpClient));
   }
 
   @Override
   public ClientResponse handle(ClientRequest cr)
       throws ClientHandlerException {
-    return apacheHttpClientHandler.handle(cr);
+    /*
+     * Handle GET requests only as thats what we will be supporting cache for only
+     */
+    if (cr.getMethod().equals("GET")) {
+      HTTPResponse cachedResponse = cache.doCachedRequest(new HTTPRequest(cr.getURI()));
+      Headers headers = cachedResponse.getHeaders();
+      InBoundHeaders inBoundHeaders = getInBoundHeaders(headers);
+      final InputStream entity = getEntityStream(cachedResponse);
+      ClientResponse response = new ClientResponse(cachedResponse.getStatus().getCode(), inBoundHeaders, entity,
+          getMessageBodyWorkers());
+      return response;
+    }
+    /*
+     * Leave the rest to the default jersey apache http client handler to ensure number of bugs is less
+     */
+    else {
+      return apacheHttpClientHandler.handle(cr);
+    }
   }
 
   public ApacheHttpClientHandler getApacheHttpClientHandler() {
     return apacheHttpClientHandler;
+  }
+
+  public HTTPCache getCache() {
+    return cache;
+  }
+
+  public HttpClient getHttpClient() {
+    return getApacheHttpClientHandler().getHttpClient();
+  }
+
+  protected InputStream getEntityStream(HTTPResponse cachedResponse) {
+    final InputStream entity;
+    if (cachedResponse.hasPayload()) {
+      final InputStream inputStream = cachedResponse.getPayload().getInputStream();
+      if (inputStream.markSupported()) {
+        entity = inputStream;
+      }
+      else {
+        entity = new BufferedInputStream(inputStream, ReaderWriter.BUFFER_SIZE);
+      }
+    }
+    else {
+      entity = new ByteArrayInputStream(new byte[0]);
+    }
+    return entity;
+  }
+
+  protected InBoundHeaders getInBoundHeaders(Headers headers) {
+    InBoundHeaders inBoundHeaders = new InBoundHeaders();
+    Iterator<Header> headerIterator = headers.iterator();
+    while (headerIterator.hasNext()) {
+      Header header = headerIterator.next();
+      List<String> list = inBoundHeaders.get(header.getName());
+      if (list == null) {
+        list = new ArrayList<String>();
+      }
+      list.add(header.getValue());
+      inBoundHeaders.put(header.getName(), list);
+    }
+    return inBoundHeaders;
   }
 }
