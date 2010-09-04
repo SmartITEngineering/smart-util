69446135433111ecbf4a0cd9699d3eb29053ccc0
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-09-04 20:48:01 +0600

    Add client configuration related code
    There are 2 ways to get it to work. First, create a bean factory to provide
    the ClientConfig that way no need to consider anything, else create a
    properties file named user-client-config.properties in classpath root and
    it will be read as connection config; the .template is a sample.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ rest/generic-client/src/main/java/com/smartitengineering/util/rest/client/ConnectionConfig.java
@@ -0,0 +1,62 @@
+/*
+ * This is a utility project for wide range of applications
+ *
+ * Copyright (C) 2010  Imran M Yousuf (imyousuf@smartitengineering.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  10-1  USA
+ */
+package com.smartitengineering.util.rest.client;
+
+/**
+ *
+ * @author russel
+ */
+public class ConnectionConfig {
+
+    private String basicUri;
+    private int port;
+    private String host;
+    private String contextPath;
+
+  public String getHost() {
+    return host;
+  }
+
+  public void setHost(String host) {
+    this.host = host;
+  }
+
+    public String getBasicUri() {
+        return basicUri;
+    }
+
+    public void setBasicUri(String basicUrl) {
+        this.basicUri = basicUrl;
+    }
+
+    public String getContextPath() {
+        return contextPath;
+    }
+
+    public void setContextPath(String contextPath) {
+        this.contextPath = contextPath;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
