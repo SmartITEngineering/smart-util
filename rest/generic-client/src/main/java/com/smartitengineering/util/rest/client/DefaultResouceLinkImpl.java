81a5c2fe13fefb3de13e548eb825b7272f3a6563
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-09-05 06:20:09 +0600

    Add resource link API and default implementation
    This resource link API represents the whole set to quantify a resource, its
    URI, relation to the current resource and its expected MIME Type.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ rest/generic-client/src/main/java/com/smartitengineering/util/rest/client/DefaultResouceLinkImpl.java
@@ -0,0 +1,59 @@
+/*
+ * This is a utility project for wide range of applications
+ *
+ * Copyright (C) 2010  Imran M Yousuf (imyousuf@smartitengineering.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  10-1  USA
+ */
+package com.smartitengineering.util.rest.client;
+
+import java.net.URI;
+
+/**
+ *
+ * @author imyousuf
+ */
+public class DefaultResouceLinkImpl implements ResouceLink {
+
+  private URI uri;
+  private String mimeType;
+  private String rel;
+
+  public void setMimeType(String mimeType) {
+    this.mimeType = mimeType;
+  }
+
+  public void setRel(String rel) {
+    this.rel = rel;
+  }
+
+  public void setUri(URI uri) {
+    this.uri = uri;
+  }
+
+  @Override
+  public URI getUri() {
+    return uri;
+  }
+
+  @Override
+  public String getMimeType() {
+    return mimeType;
+  }
+
+  @Override
+  public String getRel() {
+    return rel;
+  }
+
+}
