e369625156cb4f8dcc71e0c8834a419d3311635e
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-09-06 06:55:32 +0600

    Start adding Google Guice support for Smart Bean Util
    Add the project structure along with dependency to Google Guice.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ smart-bean-guice-util/src/main/java/com/smartitengineering/util/bean/guice/App.java
@@ -0,0 +1,13 @@
+package com.smartitengineering.util.bean.guice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
