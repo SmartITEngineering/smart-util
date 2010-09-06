0b1ab23d11fe517c9a6206ea827f7e7a65787906
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-09-06 08:57:48 +0600

    Add bean class to contains bean signature
    In accordance to API also fix the spring implementation
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- smart-bean-spring-util/src/main/java/com/smartitengineering/util/bean/spring/SpringBeanFactory.java
+++ smart-bean-spring-util/src/main/java/com/smartitengineering/util/bean/spring/SpringBeanFactory.java
@@ -38,7 +38,7 @@ public class SpringBeanFactory
         this.beanFactory = beanFactory;
     }
 
-    public boolean containsBean(String beanName)
+    public boolean containsBean(String beanName, Class beanClass)
         throws IllegalArgumentException {
         if (StringUtils.isBlank(beanName)) {
             throw new IllegalArgumentException("Bean Name can not be blank!");
@@ -51,7 +51,7 @@ public class SpringBeanFactory
         if (StringUtils.isBlank(beanName)) {
             throw new IllegalArgumentException("Bean Name can not be blank!");
         }
-        if (!containsBean(beanName)) {
+        if (!containsBean(beanName, beanClass)) {
             throw new IllegalArgumentException("No such bean in factory!");
         }
 				if(beanClass == null) {
