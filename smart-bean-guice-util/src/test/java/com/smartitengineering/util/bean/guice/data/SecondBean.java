9da3eefb42d57871bec3c9ef228890198e1afb5c
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-09-10 10:22:38 +0600

    Add test for Google Guice bean util
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ smart-bean-guice-util/src/test/java/com/smartitengineering/util/bean/guice/data/SecondBean.java
@@ -0,0 +1,44 @@
+/*
+ * This is a utility project for wide range of applications
+ *
+ * Copyright (C) 2010  Imran M Yousuf (imyousuf@smartitengineering.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  10-1  USA
+ */
+package com.smartitengineering.util.bean.guice.data;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+/**
+ *
+ * @author imyousuf
+ */
+public class SecondBean implements ISecondBean {
+
+  @Inject
+  private IThirdBean thirdBean;
+  @Inject
+  @Named("myFourthBean2")
+  private IFourthBean fourthBean;
+
+  @Override
+  public IFourthBean getFourthBean() {
+    return fourthBean;
+  }
+
+  @Override
+  public IThirdBean getThirdBean() {
+    return thirdBean;
+  }
+}
